2a57a91f3c7e3cdc042f65c6bcc61aec0ad0ca6e
ChaoYue <chaoyuejoy@gmail.com>
2015-01-13 15:45:59 +0100

    commit

--- extract_variable_from_orchidee_output.sh
+++ extract_variable_from_orchidee_output.sh
@@ -3,6 +3,9 @@
 ##Purpose:
 #This is especially for extracting varialbe values from the orchidee output file and useful for debugging purpose.
 
+##How to use:
+# ~/extract_variable_from_orchidee_output.sh in_filename out_filename string_to_remove line_number
+
 ##Arguments:
 #infile --> file from which string will be extract.
 #outfile --> final output data file, containing data sperated by blank spaces or tab.
@@ -23,12 +26,12 @@ rm -fr tempfile.txt
 
 if [ ${line_number} -eq 1 ] ; then
   grep "${search_string_and_remove}" ${infile} > tempfile.txt
-  python /home/users/ychao/python/script/remove_string_each_line.py tempfile.txt ${outfile} "${search_string_and_remove}"
+  python /homel/ychao/lscetools/remove_string_each_line.py tempfile.txt ${outfile} "${search_string_and_remove}"
   rm -fr tempfile*.txt
 else
   grep "${search_string_and_remove}" -A $((line_number-1)) ${infile} > tempfile.txt
-  python /home/users/ychao/python/script/mergeline.py tempfile.txt tempfile1.txt $((line_number+1))  #line_number needs to be added 1 because after each pattern matching, 											           #grep command add another "--" afterwards.
-  python /home/users/ychao/python/script/remove_string_each_line.py tempfile1.txt tempfile2.txt "${search_string_and_remove}"
-  python /home/users/ychao/python/script/remove_string_each_line.py tempfile2.txt ${outfile} "--"  #we have to remove the "--" that's introduced when using grep command
+  python /homel/ychao/lscetools/mergeline.py tempfile.txt tempfile1.txt $((line_number+1))  #line_number needs to be added 1 because after each pattern matching, 											           #grep command add another "--" afterwards.
+  python /homel/ychao/lscetools/remove_string_each_line.py tempfile1.txt tempfile2.txt "${search_string_and_remove}"
+  python /homel/ychao/lscetools/remove_string_each_line.py tempfile2.txt ${outfile} "--"  #we have to remove the "--" that's introduced when using grep command
   rm -fr tempfile*.txt
 fi
--- replace_line.py
+++ replace_line.py
@@ -7,9 +7,27 @@ import sys
 import subprocess
 
 input_filename = sys.argv[1]
+if input_filename in ['-h','-help']:
+    print """
+    how to use:
+    -----------
+    replace_line.py filein fileout linenumber string
+
+    Parameters:
+    -----------
+    1. string could contain multiple whitespaces.
+
+    Notes:
+    ------
+    1. filein and fileout could be the same file name, in this case filein
+        will be overwritten.
+
+    """
+    sys.exit()
 output_filename = sys.argv[2]
 insert_line_number = int(sys.argv[3])
-insert_string = sys.argv[4]
+insert_string = sys.argv[4:]
+insert_string = ' '.join(insert_string)
 
 overwrite = False
 if input_filename == output_filename:
@@ -23,11 +41,10 @@ fob = open(input_filename,'r')
 fob2 = open(output_filename,'w')
 
 for lnum, line in enumerate(fob):
-    fob2.write(line)
-    if lnum == insert_line_number-1:
-        insert_string_list = insert_string.split('\n')
-        for substr in insert_string_list:
-            fob2.write(substr+'\n')
+    if lnum != insert_line_number-1:
+        fob2.write(line)
+    else:
+        fob2.write(insert_string+'\n')
 
 fob.close()
 fob2.close()
